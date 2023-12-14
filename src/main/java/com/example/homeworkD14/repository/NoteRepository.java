8e3146f7e67ddcedd9ec6b47d05eb6dee271fa83
Тимченко Влад <timchenko.vld@gmail.com>
2023-12-14 20:49:21 +0200

    added repository annotation

--- src/main/java/com/example/homeworkD14/repository/NoteRepository.java
+++ src/main/java/com/example/homeworkD14/repository/NoteRepository.java
@@ -2,8 +2,10 @@ package com.example.homeworkD14.repository;
 
 import com.example.homeworkD14.entity.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface NoteRepository extends JpaRepository<NoteEntity, UUID> {
 }
